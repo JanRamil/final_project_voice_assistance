863f20f0b409b7886c2123a95c9595fa14efb9b6
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-02-09 05:44:37 +0800

    Code that will associate with letters and digits

--- final_python_project.py
+++ final_python_project.py
@@ -10,3 +10,7 @@ def generate_password(min_length_entry, number_var, special_var, result_label):
     min_length = int(min_length_entry.get())
     has_number = number_var.get()
     has_special = special_var.get()
+    # For letters, digits, and special
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
