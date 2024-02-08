f5f171f93f0e254a4b63387799b3e502d092d924
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-02-09 05:43:17 +0800

    Importing tkinter as module

--- final_python_project.py
+++ final_python_project.py
@@ -1,5 +1,12 @@
-# Final Python Project
-# Automate Hill Climb Racing Game Using Python
+# Final Project
+# Create a program that will generate a strong password
 
-# Creating pseudocode
+import tkinter as tk
+import random
+import string
 
+# Create a function
+def generate_password(min_length_entry, number_var, special_var, result_label):
+    min_length = int(min_length_entry.get())
+    has_number = number_var.get()
+    has_special = special_var.get()
